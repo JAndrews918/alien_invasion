7fd8451691cc6941d5fdaa74805e3eae3fb77cb2
Jamie Andrews <jamie.andrews918@gmail.com>
2025-06-02 14:40:39 -0400

    Add move right for ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,19 +24,23 @@ class AlienInvasion:
             self._update_screen()          
             self.clock.tick(60)
 
-def _check_events(self):
-    """Respond to keypresses and mouse events"""
-    for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+                    elif event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RIGHT:
+                            # Move the ship to the right
+                            self.ship.rect.x += 1
 
-def _update_screen(self):
-     """Update images on the screen, and flip to the new screen."""
-     self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
